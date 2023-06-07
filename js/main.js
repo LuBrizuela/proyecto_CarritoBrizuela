f80de112758a35bfffb145a3dde082676d7a62b6
Luciana Brizuela <lusofibri@gmail.com>
2023-06-07 14:52:07 -0300

    cambios en el archivo js para entrega 2

--- js/main.js
+++ js/main.js
@@ -1,145 +1,46 @@
 alert("Bienvenido al hotel Amerian");
 
 
-//let cantidadPersonas = prompt("por favor, ingrese la cantidad de personas a hospedarse \n1 - 1 \n2 - 2 \n3 - 3 \n4 - 4 \nPresiona x para cancelar la reserva")
+let opcion = prompt('ingrese N si desea realizar una nueva reserva. \n ingrese B si desea buscar una reserva \nIngrese X si desea cancelar la reserva');
 
+const reserva = [
+    {nombre: "luciana", cantPer: 2, fecha: "18/09"},
+    {nombre: "gonzalo", cantPer: 4, fecha: "11/07"}
+];
 
-// function reservaConfirmada(personas, tipoHab, fechas) {
-//      alert("su reserva ha sido confirmada. La cantidad de personas es " + personas + " el tipo de habitacion es habitacion " + tipoHab + " y la fecha es " + fechas);
-//  }
-
-//funcion constructora de la reserva
-
-function reserva(nombre, fecha, cantper) {
+function Reserva(nombre, cantPer, fecha) {
     this.nombre = nombre;
+    this.cantPer = cantPer;
     this.fecha = fecha;
-    this.cantper = cantper;
 }
 
-let nombre = prompt('Ingrese a nombre de quien estara la reserva. \nIngrese x si desea cancelar la reserva');
+function crearReserva() {
+    let nombre = prompt("ingrese el nombre de la persona que realiza la reserva");
+    let cantPer = prompt("ingrese la cantidad de personas que van a hospedarse");
+    let fecha = prompt("ingrese la fecha en la que va a hospedarse");
 
-if (nombre == 'x') {
-    alert('muchas gracias por su visita')
+    const nuevaReserva = new Reserva(nombre, cantPer, fecha);
+
+    return nuevaReserva;
+}
+
+function filtrar(param) {
+
+}
+
+if (opcion == "n" || opcion == "N") {
+
+    const reserva1 = crearReserva();
+    reserva.push(reserva1)
+    alert("su reservacion fue realizada con exito")
+    
+} else if (opcion == "b" || opcion == "B"){
+    let buscar = prompt("ingrese el nombre de la persona que realizo la reserva")
+    let encontrado = reserva.find((el) => { return el.nombre.includes(buscar) })
+    alert("nombre: " + encontrado.nombre + "\ncantidad de personas: " + encontrado.cantPer + "\nfecha: " + encontrado.fecha) 
+
+} else if(opcion == "x" || opcion == "X") {
+    alert("gracias por visitarnos");
 } else {
-    let fecha = prompt('ingrese la fecha en la que va a hospedarse \nIngrese x si desea cancelar la reserva');
-    if (fecha == 'x') {
-        alert('muchas gracias por su visita')
-    } else {
-        let cantidadPersonas = prompt('ingrese la cantidad de personas a hospedarse \nIngrese x si desea cancelar la reserva')
-        if (cantidadPersonas == 'x') {
-            alert('muchas gracias por su visita')
-        } else {
-            const reserva1 = new reserva(nombre, fecha, cantidadPersonas)
-
-            alert('su reserva ha sido confirmada. corrobore si los datos de la misma son correctos.')
-
-            alert("nombre: " + reserva1.nombre + "\nfecha: " + reserva1.fecha + "\nCantidad de personas: " + reserva1.cantper)
-
-            let datos = prompt('los datos ingresados son correctos? \n1- si \n2- no')
-            switch (datos) {
-                case "1":
-                    alert('muchas gracias por reservar con nosotros')
-                    break;
-
-                case "2":
-                    let correcion = prompt("ingrese el dato que desea corregir \n1 - nombre \n2 - fecha \n3 - cantidad de personas")
-
-                    switch (correcion) {
-                        case "1":
-                            let nombre = prompt("ingrese el nombre de la presona que desea reservar")
-                            reserva1.nombre = nombre
-                            alert('su reserva ha sido confirmada. estos son los datos de su reserva')
-                            alert("nombre: " + reserva1.nombre + "\nfecha: " + reserva1.fecha + "\nCantidad de personas: " + reserva1.cantper)
-
-                            alert("gracias por reservar con nosotros")
-                            break;
-
-                        case "2":
-                            let fecha = prompt("ingrese la fecha en la que va a hospedarse")
-                            reserva1.fecha = fecha
-                            alert('su reserva ha sido confirmada. estos son los datos de su reserva')
-                            alert("nombre: " + reserva1.nombre + "\nfecha: " + reserva1.fecha + "\nCantidad de personas: " + reserva1.cantper)
-
-                            alert("gracias por reservar con nosotros")
-                            break;
-
-                        case "3":
-                            let cantper = prompt("ingrese la cantidad de personas a hospedarse")
-                            reserva1.cantper = cantper
-                            alert('su reserva ha sido confirmada. estos son los datos de su reserva')
-                            alert("nombre: " + reserva1.nombre + "\nfecha: " + reserva1.fecha + "\nCantidad de personas: " + reserva1.cantper)
-
-                            alert("gracias por reservar con nosotros")
-                            break;
-                    
-                        default:
-                            break;
-                    }
-                    if (correcion == "nombre") {
-
-                    } else {
-                        if (correcion == "fecha") {
-
-                        } else {
-                            if (correcion == "cantidad") {
-                                
-                            }
-                        }
-                    }
-                    break;
-            
-                default:
-                    break;
-            }
-
-        }
-    }
+    alert("opcion no valida");
 }
-
-// while (cantidadPersonas != "x") {
-//     switch (cantidadPersonas) {
-
-//         case "1":
-
-
-//         case "2":
-//             let habitacion2 = prompt("elige el tipo de habitacion que desea: \n1 - habitacion simple con cama matrimonial \n2 - habitacion simple con camas separadas \nPresiona x para cancelar la reserva")
-//             while (habitacion2 != "x") {
-//                 let fecha2 = prompt("ingresa la fecha que desea hospedarse")
-//                 reservaConfirmada(cantidadPersonas, habitacion2, fecha2);
-//                 break;
-//             }
-//             break;
-
-//         case "3":
-//             let habitacion3 = prompt("elige el tipo de habitacion que desea: \n1 - habitacion simple con cama matrimonial \n2 - habitacion simple con camas separadas \nPresiona x para cancelar la reserva")
-//             while (habitacion3 != "x") {
-//                 let fecha3 = prompt("ingresa la fecha que desea hospedarse")
-//                 reservaConfirmada(cantidadPersonas, habitacion3, fecha3);
-//                 break;
-//             }
-//             break;
-
-//         case "4":
-//             let habitacion4 = prompt("elige el tipo de habitacion que desea: \n1 - habitacion simple con cama matrimonial \n2 - habitacion simple con camas separadas \nPresiona x para cancelar la reserva")
-//             while (habitacion3 != "x") {
-//                 let fecha4 = prompt("ingresa la fecha que desea hospedarse")
-//                 reservaConfirmada(cantidadPersonas, habitacion4, fecha4);
-//                 break;
-//             }
-//             break;
-
-
-
-//         default:
-//             alert("opcion no valida, vuelva a intentarlo.");
-//             break;
-//     }
-
-//     alert("gracias por elegirnos")
-//     break;
-
-// }
-
-
-
