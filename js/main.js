40539ee0f07f141d179f1db076f654ccf49d6ee8
Luciana Brizuela <lusofibri@gmail.com>
2023-06-14 15:33:49 -0300

    cambios en index y js

--- js/main.js
+++ js/main.js
@@ -1,46 +1,129 @@
-alert("Bienvenido al hotel Amerian");
+// variables
 
+const carrito = document.querySelector('#carrito'),
+    listaCursos = document.querySelector('#lista-cursos'),
+    contenedorCarrito = document.querySelector('#lista-carrito tbody'),
+    vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+let articulosCarrito = [];
 
-let opcion = prompt('ingrese N si desea realizar una nueva reserva. \n ingrese B si desea buscar una reserva \nIngrese X si desea cancelar la reserva');
+cargarEventListeners();
+function cargarEventListeners() {
+    //agrega un curso cuando presiona "agregar al carrito"
+    listaCursos.addEventListener('click', agregarCurso);
 
-const reserva = [
-    {nombre: "luciana", cantPer: 2, fecha: "18/09"},
-    {nombre: "gonzalo", cantPer: 4, fecha: "11/07"}
-];
+    // elimina cursos delc arrito
 
-function Reserva(nombre, cantPer, fecha) {
-    this.nombre = nombre;
-    this.cantPer = cantPer;
-    this.fecha = fecha;
+    carrito.addEventListener('click', eliminarCurso);
+
+    // vaciar carrito
+
+    vaciarCarritoBtn.addEventListener('click', () => {
+        articulosCarrito = []
+        
+        contenedorCarrito.innerHTML = ''
+    })
 }
 
-function crearReserva() {
-    let nombre = prompt("ingrese el nombre de la persona que realiza la reserva");
-    let cantPer = prompt("ingrese la cantidad de personas que van a hospedarse");
-    let fecha = prompt("ingrese la fecha en la que va a hospedarse");
+// funciones
 
-    const nuevaReserva = new Reserva(nombre, cantPer, fecha);
+function agregarCurso(e) {
+    e.preventDefault();
 
-    return nuevaReserva;
-}
-
-function filtrar(param) {
+    if (e.target.classList.contains('agregar-carrito')) {
+        const cursoSeleccionado = e.target.parentElement.parentElement
+        leerDatosCurso(cursoSeleccionado);
+    }
 
 }
+// elimina cursos
 
-if (opcion == "n" || opcion == "N") {
+function eliminarCurso(e) {
+    if (e.target.classList.contains('borrar-curso')) {
+        const cursoId = e.target.getAttribute('data-id');
+        
+        articulosCarrito = articulosCarrito.filter( curso => curso.id !== cursoId)
 
-    const reserva1 = crearReserva();
-    reserva.push(reserva1)
-    alert("su reservacion fue realizada con exito")
-    
-} else if (opcion == "b" || opcion == "B"){
-    let buscar = prompt("ingrese el nombre de la persona que realizo la reserva")
-    let encontrado = reserva.find((el) => { return el.nombre.includes(buscar) })
-    alert("nombre: " + encontrado.nombre + "\ncantidad de personas: " + encontrado.cantPer + "\nfecha: " + encontrado.fecha) 
-
-} else if(opcion == "x" || opcion == "X") {
-    alert("gracias por visitarnos");
-} else {
-    alert("opcion no valida");
+        carritoHTML();
+    }
 }
+
+
+//lee el contenido y lo extrae
+
+function leerDatosCurso(curso) {
+    //console.log(curso);
+
+    // crea un objeto con el contenido del curso
+
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h5').textContent,
+        precio: curso.querySelector('.precio').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+
+
+    }
+
+    // revisa si un elemento ya existe en el carrito
+
+    const existe = articulosCarrito.some( curso => curso.id === infoCurso.id)
+    if (existe) {
+        //
+
+        const cursos = articulosCarrito.map(curso => {
+            if(curso.id === infoCurso.id) {
+                curso.cantidad++;
+                return curso;
+            } else {
+                return curso;
+            }
+        })
+    } else {
+            //agrega elementos al arreglo de carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+    }
+
+
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
+}
+
+//muestra el carrito en el html
+
+function carritoHTML() {
+    // limpia el html
+
+    contenedorCarrito.innerHTML = ''
+
+    // recorre el carrito y genera htmkl
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+        <td>
+            <img src= "${curso.imagen}" width="100">
+        </td>
+        <td>
+            ${curso.titulo}
+        </td>
+        <td>
+            ${curso.precio}
+        </td>
+        <td>
+            ${curso.cantidad}
+        </td>
+        <td>
+            <a href="#" class="borrar-curso" data-id="${curso.id}"> X </a>
+        </td>
+
+    `;
+
+
+        // agrega el html 
+
+        contenedorCarrito.appendChild(row);
+    })
+}
+
